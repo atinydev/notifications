9b0a3d97c3a9b818140048d3085466b6f3e9e9a0
atinydev <atinydeveloper@gmail.com>
2022-03-04 08:48:40 -0600

    feat: :sparkles: Receive notifications
    React when we receive a notification

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:notifications/services/push_notifications_service.dart';
 
 import 'router/router.dart';
 import 'theme/theme.dart';
 
-class NotificationsApp extends StatelessWidget {
+class NotificationsApp extends StatefulWidget {
   const NotificationsApp({Key? key}) : super(key: key);
+
+  @override
+  State<NotificationsApp> createState() => _NotificationsAppState();
+}
+
+class _NotificationsAppState extends State<NotificationsApp> {
+  @override
+  void initState() {
+    super.initState();
+    PushNotificationService.messageStream.listen((message) {
+      print('MyApp: $message');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/services/push_notifications_service.dart
+++ lib/services/push_notifications_service.dart
@@ -1,11 +1,16 @@
 // SHA1: EE:2A:D5:5A:8C:2F:F7:6E:83:89:17:A2:FE:AC:BF:88:29:0B:D3:96
 
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 class PushNotificationService {
   static var messaging = FirebaseMessaging.instance;
   static String? token;
+  static final _messageStreamController = StreamController<String>.broadcast();
+
+  static Stream<String> get messageStream => _messageStreamController.stream;
 
   static Future<void> initializeApp() async {
     // Push Notifications
@@ -22,14 +27,24 @@ class PushNotificationService {
   }
 
   static Future<void> _onBackgroundHandler(RemoteMessage message) async {
-    print('onBackground Handler ${message.messageId}');
+    // print('onBackground Handler ${message.messageId}');
+    print(message.data);
+    _messageStreamController.add(message.notification?.body ?? 'No body');
   }
 
   static Future<void> _onMessageHandler(RemoteMessage message) async {
-    print('onMessage Handler  ${message.messageId}');
+    // print('onMessage Handler  ${message.messageId}');
+    print(message.data);
+    _messageStreamController.add(message.notification?.body ?? 'No body');
   }
 
   static Future<void> _onMessageOpenApp(RemoteMessage message) async {
-    print('onMessageOpenApp Handler ${message.messageId}');
+    // print('onMessageOpenApp Handler ${message.messageId}');
+    print(message.data);
+    _messageStreamController.add(message.notification?.body ?? 'No body');
+  }
+
+  static void closeStreams() {
+    _messageStreamController.close();
   }
 }
